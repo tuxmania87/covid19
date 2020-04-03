c11e417ec8fe85c69537fe28bf5c99c70c67532b
tuxmania87 <42938706+tuxmania87@users.noreply.github.com>
2020-04-03 09:28:01 +0200

    Add files via upload
    added World Country (all combined) and reference to data source

--- app.py
+++ app.py
@@ -7,6 +7,30 @@ import pandas as pd
 from datetime import datetime
 import numpy as np
 
+
+def create_multi_df(ddf,country_val,measure,dayfilter=True):
+    dftotal = None
+    
+    
+    for country in country_val:
+        dfc = ddf[country]
+        if dayfilter:
+            dfc = dfc[last28days:]
+        
+        dfc = dfc[[measure]]
+        dfc.columns = [measure+"_"+country]
+        
+        if dftotal is None:
+            dftotal = dfc
+        else:
+            dftotal = dftotal.join(dfc,how="outer",rsuffix="_"+country)
+
+    return dftotal
+
+def generate_data(df,countries,measure,kind="bar"):
+    return [ dict(x = df.index, y= [xx for xx in df[measure+"_"+c].values.tolist()], type=kind,name=c) for c in countries]
+
+
 todaydate = datetime.today().strftime('%Y-%m-%d')
 last14days = pd.to_datetime(todaydate, format="%Y-%m-%d") - pd.DateOffset(days=14)
 last28days = pd.to_datetime(todaydate, format="%Y-%m-%d") - pd.DateOffset(days=28)
@@ -18,36 +42,85 @@ colors = {
 
 df1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
 
+dfus = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
+
+
 d = df1.drop(["Province/State","Lat","Long"],axis=1)
+
+dworld = d.copy()
+dworld = dworld.drop(["Country/Region"],axis=1).sum()
+dworld = pd.DataFrame(dworld)
+dworld.columns = [ "World" ]
+dworld.index = pd.to_datetime(dworld.index)
+
+
 d = d.groupby(["Country/Region"]).sum().T.reset_index()
 d = d.set_index(pd.to_datetime(d["index"]))
 d = d.drop(["index"],axis=1)
+
+dfus = dfus.drop(["UID","iso2","iso3","code3","FIPS","Admin2","Lat","Long_","Combined_Key"],axis=1)
+dfus = dfus.groupby(["Country_Region"]).sum().T.reset_index()
+dfus = dfus.set_index(pd.to_datetime(dfus["index"]))
+dfus = dfus.drop(["index"],axis=1)
+
+#d = d.join(dfus,how="outer")
+
 
 dd = {}
 
 for country in d.columns:
-    dd[country] = pd.DataFrame(d[country])
-    dd[country].columns = ["Infections"]
+    c2 = "United States" if country == "US" else country 
+    dd[c2] = pd.DataFrame(d[country])
+    dd[c2].columns = ["Infections"]
 
+dd["World"] = dworld 
+dd["World"].columns = ["Infections"]
 
 df1 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 
+dfus = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
+
 d = df1.drop(["Province/State","Lat","Long"],axis=1)
+
+dworld = d.copy()
+dworld = dworld.drop(["Country/Region"],axis=1).sum()
+dworld = pd.DataFrame(dworld)
+dworld.columns = [ "World" ]
+dworld.index = pd.to_datetime(dworld.index)
+
 d = d.groupby(["Country/Region"]).sum().T.reset_index()
 d = d.set_index(pd.to_datetime(d["index"]))
 d = d.drop(["index"],axis=1)
 
 
+
+#dfus = dfus.drop(["UID","iso2","iso3","code3","FIPS","Admin2","Lat","Long_","Combined_Key","Population","Province/State"],axis=1)
+#dfus = dfus.groupby(["Country/Region"]).sum().T.reset_index()
+#dfus = dfus.set_index(pd.to_datetime(dfus["index"]))
+#dfus = dfus.drop(["index"],axis=1)
+
+#d = d.join(dfus,how="outer")
 
 for c in d.columns:
     #current data frame = dataframe[country]
     cdf = pd.DataFrame(d[c])
     cdf.columns = ["Deaths"]
-    dd[c] = dd[c].join(cdf,how="outer")
+    c2 = "United States" if c == "US" else c 
+    dd[c2] = dd[c2].join(cdf,how="outer")
+    
+dworld.columns = ["Deaths"]
+dd["World"] = dd["World"].join(dworld,how="outer")
     
 df1 =  pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
 
 d = df1.drop(["Province/State","Lat","Long"],axis=1)
+
+dworld = d.copy()
+dworld = dworld.drop(["Country/Region"],axis=1).sum()
+dworld = pd.DataFrame(dworld)
+dworld.columns = [ "World" ]
+dworld.index = pd.to_datetime(dworld.index)
+
 d = d.groupby(["Country/Region"]).sum().T.reset_index()
 d = d.set_index(pd.to_datetime(d["index"]))
 d = d.drop(["index"],axis=1)
@@ -56,7 +129,11 @@ for c in d.columns:
     #current data frame = dataframe[country]
     cdf = pd.DataFrame(d[c])
     cdf.columns = ["Recovery"]
-    dd[c] = dd[c].join(cdf,how="outer")
+    c2 = "United States" if c == "US" else c 
+    dd[c2] = dd[c2].join(cdf,how="outer")
+
+dworld.columns = ["Recovery"]
+dd["World"] = dd["World"].join(dworld,how="outer")
 
 for key in dd:
     cdf = dd[key]
@@ -119,6 +196,7 @@ app.layout = html.Div(className='container',children=[
             id='drop-countries',
             options=[{'label':i, 'value': i} for i in dd.keys()],
             value="Germany",
+            multi=True,
         ),
     ],style={'width': '20%', 'margin': 'auto','margin-bottom':'20px'}),
     
@@ -158,9 +236,14 @@ app.layout = html.Div(className='container',children=[
         dcc.Graph(id='diffdata'),
     ],className="container"),
     
+    html.Span('Detailed data of last 14 days of first selected country',
+        style={'font-weight':'bold'}),
     html.Div([
-    ],id='tabelle',className='container'),
+    ],id='tabelle',className='table-responsive'),
         
+    html.Div([
+        html.A("Data Source: https://github.com/CSSEGISandData/COVID-19",href="https://github.com/CSSEGISandData/COVID-19")
+    ],className="container",style={ 'margin-bottom':'20px'})
     
 ],style={'margin': 'auto','text-align':'center'})
 
@@ -171,11 +254,13 @@ app.layout = html.Div(className='container',children=[
      Input('yaxis-type','value'),
      Input('drop-countries','value')])
 def update_figure(selected_value, yaxis_type, country_val):
-    df_ger = dd[country_val]
+
+    if not isinstance(country_val, list):
+        country_val = [ country_val ]
+    
+    df_ger = create_multi_df(dd,country_val,selected_value,False)
     return {
-        'data': [
-            { 'x': df_ger.index, 'y': [x[0] for x in df_ger[[selected_value]].values.tolist()], 'type':'scatter','name':'pad'},
-        ],
+        'data': generate_data(df_ger,country_val,selected_value,"scatter"),
         'layout': {
             #'plot_bgcolor': colors['background'],
             #'paper_bgcolor': colors['background'],
@@ -186,7 +271,7 @@ def update_figure(selected_value, yaxis_type, country_val):
                 'type': 'linear' if yaxis_type == 'Linear' else 'log'
             },
             'title': {
-                'text': 'Timeline '+selected_value+' of '+country_val,
+                'text': 'Timeline '+selected_value+' of '+ (",".join(country_val)),
             },
         }
     }
@@ -197,26 +282,29 @@ def update_figure(selected_value, yaxis_type, country_val):
      Input('drop-countries','value'),
      Input('val-slider','value')])
 def update_diffs(vala,country_val,selected_value):
-    df_ger = dd[country_val]
-    df2 = df_ger
-    df2 = df2[last28days:]
-    
+
     measure = selected_value+" diff " + ("abs" if vala == "absolute" else "%")
     if selected_value == "Recovery":
         measure = "Infections diff " + ("abs" if vala == "absolute" else "%")
     
-    if vala == "absolute":
-        df2 = df2[[measure]]
-    else:
-        df2 = df2[[measure]]
+    if not isinstance(country_val, list):
+        country_val = [ country_val ]
+    
+    dftotal = create_multi_df(dd,country_val,measure)
+
+    df_ger = dftotal
+    df2 = df_ger
+    df2 = df2[last28days:]
+    
     
     return {
-        'data': [
-            { 'x': df2.index, 'y': [x[0] for x in df2.values.tolist()], 'type':'bar','name':'pad'},
-        ],
+        'data': generate_data(df2, country_val, measure),
+        #'data': [
+        #    { 'x': df2.index, 'y': [x[0] for x in df2.values.tolist()], 'type':'bar','name':'pad'},
+        #],
         'layout': {
             'title': {
-                'text': selected_value+' difference to prior day in '+country_val,
+                'text': selected_value+' difference to prior day in '+ (",".join(country_val)),
             },
         }
     }
@@ -225,7 +313,12 @@ def update_diffs(vala,country_val,selected_value):
     Output('tabelle','children'),
     [Input('drop-countries','value')])
 def update_table(country_val):
-    df_ger = dd[country_val]
+
+    if not isinstance(country_val,list):
+        country_val = [ country_val ]
+
+
+    df_ger = dd[country_val[0]]
     return generate_table(df_ger[last14days:]),
 
 if __name__ == '__main__':
